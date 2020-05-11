9a1ad624a0a66cce32e7e1dfd9007a93143997e6
pranavajitnair <pranavajitnair.cse18@itbhu.ac.in>
2020-05-11 22:20:00 +0530

    return f1,em

--- loss.py
+++ loss.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.distributions.categorical import Categorical as MultiNomial
 
-from utils import f1_score,get_answer
+from utils import f1_score,get_answer,exact_match_score
 
 def crossEntropyLoss(start_logits,end_logits,answer):
         lossFunction=nn.CrossEntropyLoss()
@@ -28,6 +28,7 @@ def reward(document,sampled_starts,sampled_ends,greedy_start,greedy_end,gold_sta
         rewards=[]
         gold_answer,baseline_answer=get_answer(document,gold_start,gold_end,int(greedy_start),int(greedy_end))
         baseline=f1_score(baseline_answer,gold_answer)
+        em=exact_match_score(baseline_answer,gold_answer)
         
         for i in range(4):
                 gold_answer,sample_answer=get_answer(document,gold_start,gold_end,int(sampled_starts[i]),int(sampled_ends[i]))
@@ -36,7 +37,7 @@ def reward(document,sampled_starts,sampled_ends,greedy_start,greedy_end,gold_sta
                 normalized_reward=f1-baseline
                 rewards.append(normalized_reward)
                 
-        return rewards
+        return rewards,baseline,em
     
 def loss_function_for_reinforcement(start_logits,end_logits,answer,document):
         final_start_logits=start_logits[:,-1,:].view(-1)
@@ -57,16 +58,16 @@ def loss_function_for_reinforcement(start_logits,end_logits,answer,document):
                 sample_start.append(start_sample)
                 sample_end.append(end_sample)
                 
-        rewards=reward(document,sample_start,sample_end,greedy_start,greedy_end,answer['start_offset'],answer['end_offset'])
+        rewards,baseline,em=reward(document,sample_start,sample_end,greedy_start,greedy_end,answer['start_offset'],answer['end_offset'])
         expected_reward=-sum(rewards)/len(rewards)
         
         reinforcement_loss=reinforcement_gradient_loss(start_logits,end_logits,sample_start,sample_end)
         
-        return reinforcement_loss+(expected_reward-reinforcement_loss.item())
+        return reinforcement_loss+(expected_reward-reinforcement_loss.item()),baseline,em
 
 def get_loss(start_logits,end_logits,answer,document,sigma_ce,sigma_rl):
         ce_loss=crossEntropyLoss(start_logits,end_logits,answer)
-        rl_loss=loss_function_for_reinforcement(start_logits,end_logits,document)
+        rl_loss,baseline,em=loss_function_for_reinforcement(start_logits,end_logits,document)
         
         ce_loss/=(2*sigma_ce**2)
         rl_loss/=(2*sigma_rl**2)
@@ -74,4 +75,4 @@ def get_loss(start_logits,end_logits,answer,document,sigma_ce,sigma_rl):
         ce_loss+=torch.log(sigma_ce**2)
         rl_loss+=torch.log(sigma_rl**2)
         
-        return ce_loss+rl_loss
+        return ce_loss+rl_loss,baseline,em
